7e83bd7ed7fdde09ca31ebe0e1296cfa5e490e49
StevenPurn <smp5119@gmail.com>
2018-05-10 12:11:44 -0700

    refactor basic server

--- server/basicServer.js
+++ server/basicServer.js
@@ -1,11 +1,18 @@
 /* Import node's http module: */
 const http = require('http');
-const messages = require('./request-handler');
 const utils = require('./utils');
 const url = require('url');
 
 const port = 3003;
 const ip = '127.0.0.1';
+
+const headers = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10, // Seconds.
+  'Content-Type': 'application/json'
+};
 
 const router = {
   '/restaurants/*/reviews': () => 0,
@@ -15,16 +22,18 @@ const router = {
 const matchRuleShort = (str, rule) => new RegExp(`^${rule.split('*').join('.*')}$`).test(str);
 
 const server = http.createServer((req, res) => {
+  let route = '';
   for (let i = 0; i < Object.keys(router).length; i += 1) {
-    if(matchRuleShort(url.parse(req.url).pathname), router[Object.keys(router)[i]]) {
-      route = 
+    if (matchRuleShort(url.parse(req.url).pathname, router[Object.keys(router)[i]])) {
+      route = url.parse(req.url);
     }
   }
-  const route = router[url.parse(req.url).pathname];
   if (route) {
     route(req, res);
   } else {
-    utils.sendResponse(res, '', 404);
+    const status = 404;
+    res.writeHead(status, headers);
+    res.end();
   }
 });
 
